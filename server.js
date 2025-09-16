475231487ada83da8f156ac69d4520ce236512f6
murshidmhd <murshidmuhammad65@gmail.com>
2025-09-16 16:11:53 +0530

    change in server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-// Manually enable CORS headers for all routes
+// Enable CORS
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
